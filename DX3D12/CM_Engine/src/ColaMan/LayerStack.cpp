fa15f6c050839305fa7ed94197e3d706ec2e0c65
QiZi <531387937@qq.com>
2023-05-11 17:57:34 +0800

    saved

---
+++ DX3D12/CM_Engine/src/ColaMan/LayerStack.cpp
@@ -0,0 +1,25 @@
+#include "hzpch.h"
+#include "LayerStack.h"
+
+namespace ColaMan {
+	LayerStack::LayerStack()
+	{
+		m_LayerInsert = m_Layers.begin();
+	}
+
+	LayerStack::~LayerStack()
+	{
+		for (Layer* layer : m_Layers)
+			delete layer;
+	}
+
+	void LayerStack::PushLayer(Layer* layer)
+	{
+		m_LayerInsert = m_Layers.emplace(m_LayerInsert, layer);
+	}
+
+	void LayerStack::PushOverlay(Layer* overlay)
+	{
+		m_Layers.emplace_back(overlay);
+	}
+}
